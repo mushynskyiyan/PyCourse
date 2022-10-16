1640e25ac3abec5d3754dd333a8bb6ff985b9dce
mushynskyiyan <mushinskyjandm@gmail.com>
2022-10-16 21:25:29 +0300

    33 Done

---
+++ Homework.GroupOfStudents.py
@@ -0,0 +1,73 @@
+# 1) Создайте класс, описывающий человека (создайте метод, выводящий информацию о человеке). +
+# 2) На его основе создайте класс Студент (переопределите метод вывода информации). +
+# 3) Создайте класс Группа, экземпляр которого, состоит из объектов класса Студент. +
+# Реализуйте методы добавления, +
+# удаления студента и+
+# метод поиска студента по фамилии.+
+# Метод поиска студента должен возвращать именно экземпляр класса Студент,+
+# если студент есть в группе, в противном случае - None.+
+# Определите для Группы метод __str__() для возвращения списка студентов в виде строки.+
+# Ниже представленны заготовки, которые необходимо дополнить.
+
+
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.gender} {self.age} {self.first_name} {self.last_name}'
+
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'Student: {super().__str__()} {self.record_book}'
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        res = self.find_student(last_name)
+        self.group.discard(res)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if last_name == str(student.last_name):
+                return student
+        return None
+
+    def __str__(self):
+        all_students = ""
+        for student in self.group:
+            all_students += f'{str(student)}\n'
+        return f'Number:{self.number}\n{all_students} '
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st3 = Student('Male', 33, 'Alex', 'AlexisSombrero', 'AN144')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+gr.add_student(st3)
+print(gr)
+gr.find_student('Jobs')
+gr.find_student('Jobs2')
+gr.delete_student('Jobs')
+gr.delete_student('Alexi')
+print(gr)
+
